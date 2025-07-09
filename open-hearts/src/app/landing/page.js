bd6b428e466de96e2239ba86b2c5edd2af03d6ad
heavens-potato <sswu2005@gmail.com>
2025-07-08 20:57:07 -0400

    responsive landing page and home page graphic

--- open-hearts/src/app/landing/page.js
+++ open-hearts/src/app/landing/page.js
@@ -4,61 +4,109 @@ import { useTheme } from '@mui/material/styles';
 import { Typography, Divider } from '@mui/material';
 import Image from 'next/image';
 import Logo from "../../components/images/logo.png";
+import landingGraphic from "../../components/images/landing-graphic.png"
 import ScrollableCards from "../../components/scrollableCards.js";
 import { motion } from "framer-motion";
 export default function landing() {
     const theme = useTheme();
 
     return (
-        <div className="w-full min-h-screen flex flex-col items-center">
+        <div className="w-full min-h-screen flex flex-col items-center gap-0">
 
             <motion.div className="w-full h-full flex flex-col items-center"
-                initial={{ opacity: 0, y: -100 }}
+                initial={{ opacity: 0, y: 100 }}
                 animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, ease: 'easeInOut', type: 'spring', stiffness: 100, damping: 10 }}
+                transition={{ duration: 2.0, ease: 'easeInOut', type: 'spring', damping: 10 }}
             >
                 <Image src={Logo} alt="Open Hearts logo with the tagline Spot the Scam. Save a Heart." className="w-[90%] md:w-[55%]" />
             </motion.div>
-            
-            <section className="relative z-10 flex-grow w-full flex flex-col items-center px-8 text-center bg-linear-to-b from-[#C11554] to-[#FFBF97]">
-                <div className="flex flex-col gap-8 md:gap-16 justify-center items-center z-5 w-[90%] md:w-[70%] pt-28" >
+
+            <Image src={landingGraphic} alt="landing page graphic" className="w-full block pb-0 mb-0" />
+            <section className="relative z-10 flex-grow w-full flex flex-col items-center px-8 text-center bg-linear-to-b from-[#CA2C53] to-[#FCB24D] -mt-px">
+                <div className="flex flex-col gap-8 md:gap-16 justify-center items-center z-5 w-[90%] md:w-[70%]" >
                     <div className="flex flex-col gap-4 md:gap-8">
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 0.3, delay: 0.5 }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: {
+                                        xs: theme.typography.h4.fontSize,
+                                        sm: theme.typography.h4.fontSize,
+                                        md: theme.typography.h3.fontSize,
+                                        lg: theme.typography.h3.fontSize,
+                                        xl: theme.typography.h3.fontSize
+                                    },
+                                    color: 'white',
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                OVERVIEW
+                            </Typography>
+                        </motion.div>
+                        <motion.div
+                            initial={{ opacity: 0 }}
+                            animate={{ opacity: 1 }}
+                            transition={{ duration: 0.3, delay: 0.6 }}
+                        >
+                            <Typography
+                                sx={{
+                                    fontSize: {
+                                        xs: theme.typography.h6.fontSize,
+                                        sm: theme.typography.h6.fontSize,
+                                        md: theme.typography.h4.fontSize,
+                                        lg: theme.typography.h4.fontSize,
+                                        xl: theme.typography.h4.fontSize
+                                    },
+                                    color: 'white',
+                                }}
+                            >
+                                Millions of people use online dating apps or social media to make authentic connections. Some find themselves as victims of <span className="font-black">fraud</span> instead.
+                            </Typography>
+                        </motion.div>
+                    </div>
+
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.3, delay: 0.7 }}
+                        className="w-full"
+                    >
+                        <Divider sx={{
+                            borderColor: 'white',
+                            borderBottomWidth: '4px',
+                            width: '100%',
+                            display: 'flex',
+                            justifyContent: 'center'
+                        }} />
+                    </motion.div>
+
+                    <motion.div
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        transition={{ duration: 0.3, delay: 0.8 }}
+                        className="w-full"
+                    >
                         <Typography
-                            variant="h3"
                             sx={{
+                                fontSize: {
+                                        xs: theme.typography.h4.fontSize,
+                                        sm: theme.typography.h4.fontSize,
+                                        md: theme.typography.h3.fontSize,
+                                        lg: theme.typography.h3.fontSize,
+                                        xl: theme.typography.h3.fontSize
+                                    },
                                 color: 'white',
                                 fontWeight: 'bold',
                             }}
                         >
-                            OVERVIEW
+                            EXPLORE
                         </Typography>
-                        <Typography
-                            variant="h5"
-                            sx={{
-                                color: 'white',
-                            }}
-                        >
-                            Millions of people use online dating apps or social media to make authentic connections. Some find themselves as victims of <span className="font-black">fraud</span> instead.
-                        </Typography>
-                    </div>
+                    </motion.div>
 
-                    <Divider sx={{
-                        borderColor: 'white',
-                        borderBottomWidth: '4px',
-                        width: '100%',
-                        display: 'flex',
-                        justifyContent: 'center'
-                    }} />
 
-                    <Typography
-                        variant="h3"
-                        sx={{
-                            color: 'white',
-                            fontWeight: 'bold',
-                        }}
-                    >
-                        EXPLORE
-                    </Typography>
 
                     <div>
                         {/* <ScrollableCards /> */}
--- open-hearts/src/app/page.js
+++ open-hearts/src/app/page.js
@@ -1,16 +1,19 @@
 "use client";
 
 import { useTheme, darken } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import landingImage from "../components/images/landing-image.png";
-import landing from "./landing/page";
+import logoResponsive from "../components/images/logo-responsive.png";
 import Image from 'next/image';
 import Link from 'next/link';
+import { motion } from "framer-motion";
+import { useMediaQuery } from '@mui/system';
 
 export default function Home() {
   const theme = useTheme();
+
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
   return (
     <Box
@@ -28,49 +31,65 @@ export default function Home() {
         },
       }}
     >
-      <Image src={landingImage} alt="Open Hearts logo with the tagline Spot the Scam. Save a Heart." className="w-[90%] md:w-[60%]" />
+      <motion.div
+        initial={{ opacity: 0, y: -100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2.0, ease: 'easeInOut', type: 'spring', damping: 10 }}
+        className="w-full flex justify-center"
+      >
+        <Image src={isMobile ? logoResponsive : landingImage} alt="Open Hearts logo with the tagline Spot the Scam. Save a Heart." className="w-[90%] md:w-[60%]" />
+      </motion.div>
+
 
       {/* Enter Exhibit Button */}
-      <Box sx={{
-        width: {
-          xs: '90%',
-          sm: '90%',
-          md: '90%',
-          lg: '60%',
-        }, display: "flex", justifyContent: "center"
-      }}>
-        <Button
-          fullWidth
-          sx={{
-            backgroundColor: `${theme.palette.primary.mauvePink}`,
-            color: "white",
-            fontFamily: theme.typography.fontFamily,
-            paddingY: {
-              xs: "15px",
-              sm: "15px",
-              md: "33px",
-              lg: "33px"
-            },
-            borderRadius: "77px",
-            textTransform: "none",
-            "&:hover": {
-              backgroundColor: darken(`${theme.palette.primary.mauvePink}`, 0.3), // mauve pink 
-            },
-            fontSize: {
-              xs: theme.typography.h4.fontSize,
-              sm: theme.typography.h4.fontSize,
-              md: theme.typography.h3.fontSize,
-              lg: theme.typography.h3.fontSize,
-            },
-            fontWeight: "bold",
-            textAlign: "center"
-          }}
-          component={Link}
-          href="/landing"
-        >
-          ENTER THE EXHIBIT
-        </Button>
-      </Box>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.5 }}
+        className="w-full flex justify-center"
+      >
+        <Box sx={{
+          width: {
+            xs: '90%',
+            sm: '60%',
+            md: '60%',
+            lg: '60%',
+          }, display: "flex", justifyContent: "center"
+        }}>
+          <Button
+            fullWidth
+            sx={{
+              backgroundColor: `${theme.palette.primary.mauvePink}`,
+              color: "white",
+              fontFamily: theme.typography.fontFamily,
+              paddingY: {
+                xs: "15px",
+                sm: "15px",
+                md: "33px",
+                lg: "33px"
+              },
+              borderRadius: "77px",
+              textTransform: "none",
+              "&:hover": {
+                backgroundColor: darken(`${theme.palette.primary.mauvePink}`, 0.3), // mauve pink 
+              },
+              fontSize: {
+                xs: theme.typography.h5.fontSize,
+                sm: theme.typography.h5.fontSize,
+                md: theme.typography.h3.fontSize,
+                lg: theme.typography.h3.fontSize,
+                xl: theme.typography.h3.fontSize,
+              },
+              fontWeight: "bold",
+              textAlign: "center"
+            }}
+            component={Link}
+            href="/landing"
+          >
+            ENTER THE EXHIBIT
+          </Button>
+        </Box>
+      </motion.div>
     </Box >
   );
 }
