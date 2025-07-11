b869827ca1b3e12358258fc03f0a29f0f6f296a1
Justin Glazer <jglazer365@gmail.com>
2025-07-10 20:44:19 -0400

    added code to fetch data from springboot api

--- open-hearts/src/app/minigame/page.js
+++ open-hearts/src/app/minigame/page.js
@@ -1,8 +1,22 @@
 "use client";
 
 import Header from "../../components/header.js"
+import { useEffect, useState } from "react";
 
 export default function minigame() {
+    const [data, setData] = useState(null);
+    useEffect(() =>{
+        const fetchData = async () => {
+            try{
+                const response = await fetch("http://localhost:8000/api")
+                const result = await response.text()
+                console.log(result)
+            } catch (error) {
+                console.error("Error Fetching data:", error)
+            }
+        }
+        fetchData()
+    })
 
     return (
         <div>
