0341f919af10c13347c929f89fc83cf69d88dc61
heavens-potato <sswu2005@gmail.com>
2025-07-18 20:50:07 -0400

    removed force screen height

--- open-hearts/src/app/learn/typical-case/page.js
+++ open-hearts/src/app/learn/typical-case/page.js
@@ -38,97 +38,94 @@ export default function typicalCase() {
     return (
         <div className="w-full min-h-screen flex flex-col items-center">
             <Header />
-            <div className="w-full min-h-screen">
-                <section className="w-full h-full px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 pt-13 md:pt-20 bg-linear-to-b from-[#A30B37] from-[35%] to-[#FF934F] flex flex-col items-center gap-14">
-                    <Typography
-                        sx={{
-                            fontSize: {
-                                xs: theme.typography.h4.fontSize,
-                                sm: theme.typography.h4.fontSize,
-                                md: theme.typography.h4.fontSize,
-                                lg: theme.typography.h3.fontSize,
-                                xl: theme.typography.h3.fontSize,
-                            },
-                            color: "white",
-                            fontWeight: "bold",
-                            textAlign: "center"
-                        }}
-                    >
-                        A Typical Case of Fraud
-                    </Typography>
+            <section className="w-full h-full px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 pt-13 md:pt-20 bg-linear-to-b from-[#A30B37] from-[35%] to-[#FF934F] flex flex-col items-center gap-14">
+                <Typography
+                    sx={{
+                        fontSize: {
+                            xs: theme.typography.h4.fontSize,
+                            sm: theme.typography.h4.fontSize,
+                            md: theme.typography.h4.fontSize,
+                            lg: theme.typography.h3.fontSize,
+                            xl: theme.typography.h3.fontSize,
+                        },
+                        color: "white",
+                        fontWeight: "bold",
+                        textAlign: "center"
+                    }}
+                >
+                    A Typical Case of Fraud
+                </Typography>
 
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:gap-12 pb-13 md:pb-20">
-                        {steps.map((card, index) => {
-                            const IconComponent = MuiIcons[card.icon];
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-10 md:gap-12 pb-13 md:pb-20">
+                    {steps.map((card, index) => {
+                        const IconComponent = MuiIcons[card.icon];
 
-                            return (
-                                <div key={card.number} className="flex flex-col justify-center items-center gap-2 md:gap-8">
-                                    <Typography align="center" sx={{
-                                        fontWeight: 'bold',
-                                        color: 'white',
-                                        fontSize: {
-                                            xs: theme.typography.h5.fontSize,
-                                            sm: theme.typography.h4.fontSize,
-                                            md: theme.typography.h4.fontSize,
-                                            lg: theme.typography.h3.fontSize,
-                                            xl: theme.typography.h3.fontSize,
-                                        },
-                                    }}>
-                                        {card.number}
-                                    </Typography>
-                                    <Typography align="center" sx={{
-                                        fontWeight: 'bold',
-                                        color: 'white',
-                                        fontSize: {
-                                            xs: theme.typography.h6.fontSize,
-                                            sm: theme.typography.h6.fontSize,
-                                            md: theme.typography.h6.fontSize,
-                                            lg: theme.typography.h5.fontSize,
-                                            xl: theme.typography.h5.fontSize,
-                                        },
-                                    }}>
-                                        {card.title}
-                                    </Typography>
-                                    {IconComponent && (
-                                        <IconComponent
-                                            sx={{
-                                                fontSize: {
-                                                    xs: '3rem',
-                                                    sm: '3rem',
-                                                    md: '4rem',
-                                                    lg: '4rem',
-                                                    xl: '4rem',
-                                                },
-                                                color: 'white',
-                                            }}
-                                        />
-                                    )}
-                                </div>
-                            )
-                        })}
-                    </div>
-                </section>
+                        return (
+                            <div key={card.number} className="flex flex-col justify-center items-center gap-2 md:gap-8">
+                                <Typography align="center" sx={{
+                                    fontWeight: 'bold',
+                                    color: 'white',
+                                    fontSize: {
+                                        xs: theme.typography.h5.fontSize,
+                                        sm: theme.typography.h4.fontSize,
+                                        md: theme.typography.h4.fontSize,
+                                        lg: theme.typography.h3.fontSize,
+                                        xl: theme.typography.h3.fontSize,
+                                    },
+                                }}>
+                                    {card.number}
+                                </Typography>
+                                <Typography align="center" sx={{
+                                    fontWeight: 'bold',
+                                    color: 'white',
+                                    fontSize: {
+                                        xs: theme.typography.h6.fontSize,
+                                        sm: theme.typography.h6.fontSize,
+                                        md: theme.typography.h6.fontSize,
+                                        lg: theme.typography.h5.fontSize,
+                                        xl: theme.typography.h5.fontSize,
+                                    },
+                                }}>
+                                    {card.title}
+                                </Typography>
+                                {IconComponent && (
+                                    <IconComponent
+                                        sx={{
+                                            fontSize: {
+                                                xs: '3rem',
+                                                sm: '3rem',
+                                                md: '4rem',
+                                                lg: '4rem',
+                                                xl: '4rem',
+                                            },
+                                            color: 'white',
+                                        }}
+                                    />
+                                )}
+                            </div>
+                        )
+                    })}
+                </div>
+            </section>
 
-                <Image src={BannerGraphic} alt="curved graphic on bottom of page introduction section" className="w-full h-auto" />
+            <Image src={BannerGraphic} alt="curved graphic on bottom of page introduction section" className="w-full h-auto" />
 
-                <Link href="#case-steps" className="flex flex-col items-center pt-4 pb-13">
-                    <Typography align="center" sx={{
-                        color: 'black',
-                        fontSize: theme.typography.h5.fontSize,
-                        textAlign: 'center',
-                        fontWeight: 'bold'
-                    }}>
-                        Continue
-                    </Typography>
-                    <KeyboardArrowDownIcon sx={{
-                        color: 'black',
-                        fontSize: theme.typography.h4.fontSize,
-                        textAlign: 'center',
-                        fontWeight: 'bold'
-                    }} />
-                </Link>
-            </div>
-
+            <Link href="#case-steps" className="flex flex-col items-center pt-4 pb-13">
+                <Typography align="center" sx={{
+                    color: 'black',
+                    fontSize: theme.typography.h5.fontSize,
+                    textAlign: 'center',
+                    fontWeight: 'bold'
+                }}>
+                    Continue
+                </Typography>
+                <KeyboardArrowDownIcon sx={{
+                    color: 'black',
+                    fontSize: theme.typography.h4.fontSize,
+                    textAlign: 'center',
+                    fontWeight: 'bold'
+                }} />
+            </Link>
 
             <section className="w-full min-h-screen px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32" id="case-steps">
                 <h1>TYPICAL CASE BLEH BLEH BLEH</h1>
