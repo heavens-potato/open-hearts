0078b73fee9f9201f154896d4e6f46b70988f79e
heavens-potato <sswu2005@gmail.com>
2025-07-25 19:25:26 -0400

    removed extra imports

--- open-hearts/src/components/header.js
+++ open-hearts/src/components/header.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef, useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/system';
 import Image from 'next/image';
