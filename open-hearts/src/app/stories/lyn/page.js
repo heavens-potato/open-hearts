023f1ea9400e3bbdf9c7560f8602d226153ef6e7
heavens-potato <sswu2005@gmail.com>
2025-07-29 09:48:40 -0400

    Lyn story page

--- open-hearts/src/app/stories/lyn/page.js
+++ open-hearts/src/app/stories/lyn/page.js
@@ -1,5 +1,6 @@
 "use client";
 import Header from "../../../components/header.js";
+import StoryTab from "../../../components/storyTab.js";
 import { motion } from "framer-motion";
 import { Typography } from "@mui/material";
 import Link from 'next/link';
@@ -8,12 +9,92 @@ import { useTheme } from "@mui/material/styles";
 export default function Lyn() {
     const theme = useTheme();
 
+    const summary = [
+        <>
+            <Typography
+                sx={{
+                    fontSize: theme.typography.h6.fontSize,
+                    color: "black",
+                }}
+            >
+                After meeting online in 2020, the two engaged in an <span className="font-bold">intimate, online-only relationship</span>.
+                Derek claimed to Lyn that he had recently <span className="font-bold">moved from Manchester to Dubai</span> where he supposedly owned a mineral company.
+                He used <span className="font-bold">edited pictures</span> stolen from the internet to convince Lyn that he, and the relationship, were genuine.
+            </Typography>
+        </>,
+        <>
+            <Typography
+                sx={{
+                    fontSize: theme.typography.h6.fontSize,
+                    color: "black",
+                }}
+            >
+                As their relationship deepened, Derek began requesting money from Lyn.
+                He claimed to be using them for <span className="font-bold<">medical</span> and <span className="font-bold">business-related</span> expenses.
+                He also pushed her to open <span className="font-bold">multiple cryptocurrency accounts</span> to send money to others that he claimed to be his business associates.
+            </Typography>
+        </>,
+        <>
+            <Typography
+                sx={{
+                    fontSize: theme.typography.h6.fontSize,
+                    color: "black",
+                }}
+            >
+                Lyn genuinely believed she was helping Derek with urgent medical bills and business expenses, but it was all a <span className="font-bold">calculated lie</span>.
+                She was being expertly taken advantage of and was ultimately defrauded <span className="font-bold">over £50,000</span>, including her <span className="font-bold">entire pension</span>.
+            </Typography>
+        </>
+    ]
+
+    const redFlags = [
+        <>
+            <Typography
+                sx={{
+                    fontSize: theme.typography.h6.fontSize,
+                    color: "black",
+                }}
+            >
+                <span className="font-bold">Online-Only Relationship and Avoidance of Meetings: </span>
+                Derek claimed to be abroad for the duration of the relationship, preventing Lyn from meeting him in person to verify his identity.
+                The entirely online nature of their relationship made it easy for him to hide his true self.
+            </Typography>
+        </>,
+        <>
+            <Typography
+                sx={{
+                    fontSize: theme.typography.h6.fontSize,
+                    color: "black",
+                }}
+            >
+                <span className="font-bold">Urgent Requests for Financial Help: </span>
+                Using excuses such as medical and business-related expenses, Derek was able to manufacture various urgent scenarios to convince Lyn to transfer large sums of money.
+            </Typography>
+        </>,
+        <>
+            <Typography
+                sx={{
+                    fontSize: theme.typography.h6.fontSize,
+                    color: "black",
+                }}
+            >
+                <span className="font-bold">Pressure to use Cryptocurrency Accounts: </span>
+                Encouraging Lyn to open multiple cryptocurrency accounts and send money to various "associates" is a major red flag. Scammers often prefer cryptocurrencies as well as having multiple receiving accounts as they are more difficult to track.
+            </Typography>
+        </>
+    ]
+
     return (
         <div className="w-full min-h-screen flex flex-col items-center justify-between">
             <Header />
 
-            <section className="flex flex-row gap-4 w-full h-full px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32">
-                <div className="flex flex-col gap-4 w-1/3">
+            <section className="flex flex-col md:flex-row gap-4 w-full h-full px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 items-center justify-center mb-13">
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.0, delay: 0.1 }}
+                    className="flex flex-col gap-4 w-full md:w-1/3 h-full"
+                >
                     <Typography
                         sx={{
                             fontSize: {
@@ -43,17 +124,22 @@ export default function Lyn() {
                     >
                         Lyn, a woman in her 60s from Warrington, England, became a victim of romance fraud, losing over £50,000 to a scammer named "Derek."
                     </Typography>
-                </div>
-                <div className="">
-
-                </div>
+                </motion.div>
+                <motion.div
+                    className="w-full md:w-2/3"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.0, delay: 0.3 }}
+                >
+                    <StoryTab summary={summary} redFlags={redFlags} />
+                </motion.div>
             </section>
 
             <motion.div
                 className="w-full flex justify-between px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 pb-13 md:pb-18"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ duration: 1.0, delay: 0.4 }}
+                transition={{ duration: 1.0, delay: 0.5 }}
             >
                 <Link href="/stories" passHref >
                     <Typography
---
+++ open-hearts/src/components/storyTab.js
@@ -0,0 +1,205 @@
+"use client";
+
+import { useState } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Typography } from "@mui/material";
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import FlagIcon from '@mui/icons-material/Flag';
+import { motion, AnimatePresence } from "framer-motion";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandLessIcon from '@mui/icons-material/ExpandLess';
+import { useMediaQuery } from '@mui/system';
+
+export default function StoryTab({ summary, redFlags }) {
+    const theme = useTheme();
+
+    const [openIndex, setOpenIndex] = useState(0);
+    const isMobile = useMediaQuery(theme.breakpoints.down('md'));
+
+    return (
+        <div>
+            {
+                isMobile ? (
+                    <div className="w-full h-full flex flex-col gap-4">
+                        <div className="w-full flex flex-row justify-center items-center bg-[#A33E70] px-16 py-6" onClick={() => setOpenIndex(0)}>
+                            <Typography
+                                sx={{
+                                    color: 'white',
+                                    fontSize: {
+                                        xs: theme.typography.h6.fontSize,
+                                        sm: theme.typography.h6.fontSize,
+                                        md: theme.typography.h6.fontSize,
+                                        lg: theme.typography.h5.fontSize,
+                                        xl: theme.typography.h5.fontSize,
+                                    },
+                                    textAlign: "center",
+                                    fontWeight: openIndex == 0 ? "bold" : "normal",
+                                }}
+                            >
+                                Story Details
+                            </Typography>
+                            {
+                                openIndex == 0 ? <ExpandLessIcon
+                                    sx={{
+                                        fontSize: '3rem',
+                                        color: 'white'
+                                    }}
+                                /> : <ExpandMoreIcon
+                                    sx={{
+                                        fontSize: '3rem',
+                                        color: 'white'
+                                    }}
+                                />
+                            }
+                        </div>
+                        {
+                            openIndex === 0 &&
+                            <ul className="list-disc pl-6 py-6">
+                                {summary.map((item, index) => (
+                                    <li key={index}>
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        }
+                        <div className="w-full flex flex-row justify-center items-center bg-[#A30B37] px-16 py-6" onClick={() => setOpenIndex(1)}>
+                            <Typography
+                                sx={{
+                                    color: 'white',
+                                    fontSize: {
+                                        xs: theme.typography.h6.fontSize,
+                                        sm: theme.typography.h6.fontSize,
+                                        md: theme.typography.h6.fontSize,
+                                        lg: theme.typography.h5.fontSize,
+                                        xl: theme.typography.h5.fontSize,
+                                    },
+                                    textAlign: "center",
+                                    fontWeight: openIndex == 1 ? "bold" : "normal",
+                                }}
+                            >
+                                Red Flags
+                            </Typography>
+                            {
+                                openIndex == 1 ? <ExpandLessIcon
+                                    sx={{
+                                        fontSize: '3rem',
+                                        color: 'white'
+                                    }}
+                                /> : <ExpandMoreIcon
+                                    sx={{
+                                        fontSize: '3rem',
+                                        color: 'white'
+                                    }}
+                                />
+                            }
+                        </div>
+                        {
+                            openIndex === 1 &&
+                            <ul className="list-disc pl-6 py-6">
+                                {redFlags.map((item, index) => (
+                                    <li key={index}>
+                                        {item}
+                                    </li>
+                                ))}
+                            </ul>
+                        }
+                    </div>
+                ) : (
+                    <div className="w-full flex flex-col items-center justify-center">
+                        <div className="flex flex-row gap-2 justify-evenly w-full">
+                            <div
+                                className={`flex flex-row gap-4text-center justify-center items-center w-1/2 ${openIndex === 0 ? 'rounded-b-none h-20 shadow-none' : 'rounded-b-2xl h-18 shadow-xl'} rounded-t-2xl cursor-pointer bg-[#A33E70]`}
+                                onClick={() => setOpenIndex(0)}
+                            >
+                                <PersonSearchIcon
+                                    sx={{
+                                        fontSize: {
+                                            xs: theme.typography.h5.fontSize,
+                                            sm: theme.typography.h5.fontSize,
+                                            md: theme.typography.h5.fontSize,
+                                            lg: theme.typography.h4.fontSize,
+                                            xl: theme.typography.h4.fontSize,
+                                        },
+                                        color: "white",
+                                    }}
+                                />
+                                <Typography
+                                    sx={{
+                                        fontSize: {
+                                            xs: theme.typography.h6.fontSize,
+                                            sm: theme.typography.h6.fontSize,
+                                            md: theme.typography.h6.fontSize,
+                                            lg: theme.typography.h5.fontSize,
+                                            xl: theme.typography.h5.fontSize,
+                                        },
+                                        color: "white",
+                                        fontWeight: "bold"
+                                    }}
+                                >
+                                    Story Details
+                                </Typography>
+                            </div>
+                            <div
+                                className={`flex flex-row gap-4 bg-[#A30B37] text-center justify-center items-center w-1/2 ${openIndex === 1 ? 'rounded-b-none h-20 shadow-none' : 'rounded-b-2xl h-18 shadow-xl'} rounded-t-2xl cursor-pointer`}
+                                onClick={() => setOpenIndex(1)}
+                            >
+                                <FlagIcon
+                                    sx={{
+                                        fontSize: {
+                                            xs: theme.typography.h5.fontSize,
+                                            sm: theme.typography.h5.fontSize,
+                                            md: theme.typography.h5.fontSize,
+                                            lg: theme.typography.h4.fontSize,
+                                            xl: theme.typography.h4.fontSize,
+                                        },
+                                        color: "white",
+                                    }}
+                                />
+                                <Typography
+                                    sx={{
+                                        fontSize: {
+                                            xs: theme.typography.h6.fontSize,
+                                            sm: theme.typography.h6.fontSize,
+                                            md: theme.typography.h6.fontSize,
+                                            lg: theme.typography.h5.fontSize,
+                                            xl: theme.typography.h5.fontSize,
+                                        },
+                                        color: "white",
+                                        fontWeight: "bold"
+                                    }}
+                                >
+                                    Red Flags
+                                </Typography>
+                            </div>
+                        </div>
+                        <div className="w-full h-full overflow-y-scroll border-[#A33E70] border-4 rounded-b-2xl p-6 scrollbar-hide shadow-xl">
+                            {
+                                openIndex === 0
+                                    ?
+                                    <ul
+                                        className="list-disc pl-6 gap-4"
+                                    >
+                                        {summary.map((item, index) => (
+                                            <li key={index}>
+                                                {item}
+                                            </li>
+                                        ))}
+                                    </ul>
+                                    :
+                                    <ul className="list-disc pl-6 gap-4">
+                                        {redFlags.map((item, index) => (
+                                            <li key={index}>
+                                                {item}
+                                            </li>
+                                        ))}
+                                    </ul>
+                            }
+                        </div>
+                    </div>
+                )
+
+            }
+
+        </div>
+    )
+}
