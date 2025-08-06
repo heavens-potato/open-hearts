a5be0af02643eff2aefbb10982e8c4741160ef40
Justin Glazer <jglazer365@gmail.com>
2025-08-06 18:08:39 -0400

    styled message pop up map function for minigame

--- open-hearts/src/components/game.js
+++ open-hearts/src/components/game.js
@@ -2,14 +2,16 @@
 
 import { useState } from "react";
 import { useTheme } from "@mui/material/styles";
-import { Typography} from "@mui/material";
+import { Typography, Box} from "@mui/material";
 import Image from "next/image";
 import blankProfilePic from "../components/images/blank-pfp.png";
 
 export default function Game({ gameStarted, currProfile }) {
     const theme = useTheme()
     const [page, setPage] = useState(0);
+    const [messageArr, setMessageArr] = useState(["test 1", "test2", "test3"])
     let count = 0;
+    let messageInd = 0;
 
     // Page 0: Match page
     if (page === 0) {
@@ -106,9 +108,28 @@ export default function Game({ gameStarted, currProfile }) {
 
     if (page == 2) {
         return (
-            <>
-                <h1>NEXT PAGE</h1>
-            </>
+            <div className="h-[37rem] w-[19rem] border-gray-500 border-4 rounded-4xl">
+                <div className="h-[37rem] bg-white border-black border-8 rounded-3xl flex flex-col">
+                    <div className="flex flex-row justify-center items-center mt-10"> {/* Header */}
+                        <Image
+                            src={blankProfilePic}
+                            alt="Blank Profile Picture"
+                            width={50}
+                            height={50}
+                            className="rounded-full mr-1"
+                        />
+                        <Typography sx={{fontSize: '0.9rem'}}>Chat with <strong>{currProfile.name}</strong> </Typography>   {/* Header End */}                     
+                    </div>
+                    <div className="flex flex-col mt-5"> {/* Message */}
+                        {messageArr.map((text) => (   
+                            <div style={{display: 'flex', flexDirection: 'row', justifyContent: messageInd %2 === 0 ? 'flex-start' : 'flex-end', 
+                                marginLeft: messageInd %2 ===0 ? '1.75rem' : '0', marginRight: messageInd %2 ===0 ? '0' : '1.75rem', }} key={count++}>
+                                <Typography sx={{textAlign: messageInd++ %2 === 0 ? 'left' : 'right', marginBottom: '0.8rem', padding: '0.5rem 0.5rem 0.5rem 0.5rem', width:'50%', right: '0', borderRadius: '0.2rem', color: 'white', bgcolor: '#7D1538', fontSize: '0.75rem'}}>{text}</Typography>
+                            </div>
+                        ))}
+                    </div> {/* Message End */}
+                </div>
+            </div>
         )
     }
 }
