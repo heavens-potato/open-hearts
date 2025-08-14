d41aedfc0ec247ef59e3374a003ae12c77de6007
Justin Glazer <jglazer365@gmail.com>
2025-08-13 22:10:25 -0400

    finished styling dialogue choice

--- open-hearts/src/components/game.js
+++ open-hearts/src/components/game.js
@@ -11,8 +11,10 @@ export default function Game({ gameStarted, currProfile, dialogue }) {
     const theme = useTheme()
     const [page, setPage] = useState(0);
     const [messageArr, setMessageArr] = useState([]);
+    const tempOptions = ["Option1", "Option2", "Option3"];
     let count = 0;
     let messageInd = 0;
+    let optionCount = 1;
     let introMessage = "";
 
     if (currProfile && currProfile.isScammer) {
@@ -156,8 +158,16 @@ export default function Game({ gameStarted, currProfile, dialogue }) {
                             <Typography sx={{textAlign: messageInd++ %2 === 0 ? 'left' : 'right', marginBottom: '0.8rem', padding: '0.5rem 0.5rem 0.5rem 0.5rem', width:'50%', right: '0', borderRadius: '0.2rem', color: 'white', bgcolor: '#7D1538', fontSize: '0.75rem'}}>{introMessage}</Typography>
                         </div>}
                     </div> {/* Message End */}
-                    <div className="h-1/2">
-                        <Typography sx={{fontSize: '0.75rem', borderRadius: '0.25rem', textAlign: 'center', color: 'white', backgroundColor: '#A33E70', padding: '0.5rem 0 0.5rem 0', margin: '0 1.5rem 0 1.5rem'}}>Choose a Response ...</Typography>
+                    <div className="h-1/2"> {/* Choice start */}
+                        <Typography sx={{fontSize: '0.75rem', borderRadius: '0.25rem', textAlign: 'center', color: 'white', backgroundColor: '#A33E70', padding: '0.5rem 0 0.5rem 0', margin: '0 1.5rem 1rem 1.5rem'}}>Choose a Response ...</Typography>
+                        {tempOptions.map((option) => (
+                            <div className="cursor-pointer rounded-sm flex-col justify-center border-2 border-[#A33E70] mb-1.5 mx-6.5" key={count++}>
+                                <Typography sx={{padding: "0.35rem 0.35rem 0.35rem 0.35rem", fontSize: '0.65rem', color: 'black'}}>{optionCount++}. {option}</Typography>
+                            </div>
+                        ))}
+                        <div>
+
+                        </div>
                     </div>
                 </div>
             </div>
