0162e0c0fa1693441fdfcbe9d1e7d19bd972722d
heavens-potato <sswu2005@gmail.com>
2025-08-19 17:16:35 +0800

    updated story page landing animations

--- open-hearts/src/app/stories/page.js
+++ open-hearts/src/app/stories/page.js
@@ -12,13 +12,13 @@ export default function stories() {
     return (
         <div className="w-full min-h-screen flex flex-col items-center justify-between">
             <Header />
-            <motion.section
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                transition={{ duration: 1.0, delay: 0.1 }}
-                className="w-full flex flex-col items-center mt-20 mb-40 px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 gap-8"
-            >
-                <div className="w-full h-full border-t-4 border-b-4 border-[#A30B37] flex flex-col py-12 px-6 md:px-12 justify-center items-center gap-4">
+            <section className="w-full flex flex-col items-center mt-20 mb-40 px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 gap-8">
+                <motion.div
+                    className="w-full h-full border-t-4 border-b-4 border-[#A30B37] flex flex-col py-12 px-6 md:px-12 justify-center items-center gap-4"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.0, delay: 0.1, ease: "easeInOut" }}
+                >
                     <Typography
                         sx={{
                             fontSize: {
@@ -48,52 +48,59 @@ export default function stories() {
                     >
                         Explore real stories of romance fraud and identify major red flags.
                     </Typography>
-                </div>
-                <Button
-                    sx={{
-                        backgroundColor: `${theme.palette.primary.main}`,
-                        color: "white",
-                        fontFamily: theme.typography.fontFamily,
-                        paddingY: {
-                            xs: "15px",
-                            sm: "15px",
-                            md: "33px",
-                            lg: "33px"
-                        },
-                        borderRadius: "77px",
-                        textTransform: "none",
-                        "&:hover": {
-                            backgroundColor: darken(`${theme.palette.primary.main}`, 0.3), // mauve pink 
-                        },
-                        fontSize: {
-                            xs: theme.typography.h5.fontSize,
-                            sm: theme.typography.h5.fontSize,
-                            md: theme.typography.h5.fontSize,
-                            lg: theme.typography.h4.fontSize,
-                            xl: theme.typography.h4.fontSize,
-                        },
-                        fontWeight: "bold",
-                        textAlign: "center",
-                        width: {
-                            xs: '100%',
-                            sm: '100%',
-                            md: '70%',
-                            lg: '70%',
-                            xl: '70%',
-                        }
-                    }}
-                    component={Link}
-                    href="/stories/lyn"
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.0, delay: 0.5, ease: "easeInOut" }}
+                    className="w-full flex justify-center items-center h-full"
                 >
-                    START READING
-                </Button>
-            </motion.section>
+                    <Button
+                        sx={{
+                            backgroundColor: `${theme.palette.primary.main}`,
+                            color: "white",
+                            fontFamily: theme.typography.fontFamily,
+                            paddingY: {
+                                xs: "15px",
+                                sm: "15px",
+                                md: "33px",
+                                lg: "33px"
+                            },
+                            borderRadius: "77px",
+                            textTransform: "none",
+                            "&:hover": {
+                                backgroundColor: darken(`${theme.palette.primary.main}`, 0.3), // mauve pink 
+                            },
+                            fontSize: {
+                                xs: theme.typography.h5.fontSize,
+                                sm: theme.typography.h5.fontSize,
+                                md: theme.typography.h5.fontSize,
+                                lg: theme.typography.h4.fontSize,
+                                xl: theme.typography.h4.fontSize,
+                            },
+                            fontWeight: "bold",
+                            justifyContent: "center",
+                            width: {
+                                xs: '100%',
+                                sm: '100%',
+                                md: '70%',
+                                lg: '70%',
+                                xl: '70%',
+                            }
+                        }}
+                        component={Link}
+                        href="/stories/lyn"
+                    >
+                        START READING
+                    </Button>
+                </motion.div>
+            </section>
 
             <motion.div
                 className="w-full flex justify-start px-6 sm:px-10 md:px-16 lg:px-24 xl:px-32 pb-13 md:pb-18"
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
-                transition={{ duration: 1.0, delay: 0.4 }}
+                transition={{ duration: 1.0, delay: 0.5 }}
             >
                 <Link href="/learn/typical-case" passHref >
                     <Typography
